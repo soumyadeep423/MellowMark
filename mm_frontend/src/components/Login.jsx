fbeac5d127e3436a6a13274bd2384899f160f6ee
soumyadeep423 <soumyadeep.das423@gmail.com>
2025-03-10 00:09:31 +0530

    reverted to localhost

--- mm_frontend/src/components/Login.jsx
+++ mm_frontend/src/components/Login.jsx
@@ -16,7 +16,7 @@ export default function Login() {
         }
 
         try {
-            const response = await fetch("https://mellowmark.onrender.com/auth/login", {
+            const response = await fetch("https://localhost:8000/auth/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ username, password }),
--- server.js
+++ server.js
@@ -12,12 +12,7 @@ const { router: authRoutes, authenticateUser } = require("./auth");
 app.use("/auth", authRoutes);
 
 // MongoDB Connection
-mongoose.connect("mongodb+srv://soumyadeepdas423:3wog6Zae4KNSn2tM@phantom.xvnjd.mongodb.net/?retryWrites=true&w=majority&appName=Phantom",
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        tlsAllowInvalidCertificates: true, // Add this option
-    });
+mongoose.connect("mongodb://localhost:27017");
 
 // MongoDB Document Schema (Per User)
 const DocumentSchema = new mongoose.Schema({
