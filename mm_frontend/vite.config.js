c76fcc83d67e0a3c05eb2d994fdbdad013b7035e
soumyadeep423 <soumyadeep.das423@gmail.com>
2025-03-09 23:30:26 +0530

    fixed vercel issue

--- mm_frontend/vite.config.js
+++ mm_frontend/vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "dist", // Ensure output directory is set correctly
-  },
-})
+    plugins: [react()],
+    build: { outDir: "dist" },
+    server: {
+        historyApiFallback: true, // Ensures Vercel handles React routes correctly
+    },
+});
